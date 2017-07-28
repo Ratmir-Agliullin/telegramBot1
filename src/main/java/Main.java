65e74f19406f5e7de45514f9d384f1003cea2a65
agliullin <ratmir-91@mail.ru>
2017-07-28 14:40:17 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,54 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Main());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help"))
+                sendMsg(message, "Привет, я робот");
+            else
+                sendMsg(message, "Я не знаю что ответить на это");
+        }
+    }
+
+    public String getBotUsername() {
+        return "ByRatmirBot";
+    }
+
+    public String getBotToken() {
+        return "380549679:AAFu5Bv95PatkyLxlYtWA8Nxdgjvqghb5GA";
+    }
+
+    public void onClosing() {
+
+    }
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
